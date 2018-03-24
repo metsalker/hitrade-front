5dee4ec8258d713419f544076fe8cac68e48d397
Metsalker <paranoiasdoida@gmail.com>
2018-03-24 21:48:57 +0000

    add product list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import style from "./_scss/main.scss";
+
+// Start Products List //
+$(document).ready(function(){
+
+  $("#price-range").ionRangeSlider({
+    type: "double",
+    grid: true,
+    prefix:'$',
+    min: -0,
+    max: 300,
+    from: 30,
+    to: 100
+});
+
+//   active color
+  $('.color-list .color').on("click",function(){
+    $('.color-list .color').removeClass("active");
+    $(this).addClass("active");
+  });
+});
